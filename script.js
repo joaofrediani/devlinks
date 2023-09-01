41c79292780d64d78060f02eda03e67a1b876fbe
“joaofrediani” <email>
2023-09-01 10:27:25 -0300

    :tada: Primeiro commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function ToggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("modo-claro")
+
+  // const img = document.querySelector('#perfil img')
+
+  // if (html.classList.contains("modo-claro")) {
+
+  //   img.setAttribute("src", "./assets/nomedafoto.png")
+
+  // } else {
+
+  //   img.setAttribute("src", "./assets/avataratual.png")
+
+  // }
+
+  // if(html.classList.contains('modo-claro')) {
+
+  //     html.classList.remove('modo-claro')
+
+  // } else {
+
+  //   html.classList.add("modo-claro")
+
+  // }
+}
